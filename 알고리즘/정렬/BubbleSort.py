71643edde8e2b1324b3e7eee3f6941204374c166
jaehoon <dkekdmf@naver.com>
2024-07-09 00:19:46 +0900

    2024-07-09 error solution

---
+++ 알고리즘/정렬/BubbleSort.py
@@ -0,0 +1,16 @@
+import random
+
+def bubblesort(data):
+    for i in range(len(data)-1):
+        swap = False
+        for j in range(len(data)-i-1):
+            if data[j]>data[j+1]:
+                data[j],data[j+1]=data[j+1],data[j]
+                swap = True
+        if swap ==False:
+            break
+    return data
+
+data_list = random.sample(range(100),10)
+
+print(bubblesort(data_list))
